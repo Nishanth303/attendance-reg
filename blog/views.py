834760c6e84b811272a12e2b251da01703db0f65
admin <303nishanth@gmail.com>
2016-09-24 12:55:52 +0530

    login page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+from .models import Post
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
-def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+def home_page(request):
+	return render(request, 'blog/home_page.html', {})
